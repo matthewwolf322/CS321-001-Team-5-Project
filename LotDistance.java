b7b5f557f3d08283ae176cc51d357383f81d3629
sputla2 <56198446+sputla2@users.noreply.github.com>
2019-11-04 17:07:51 -0500

    Initial update for LotDistance.java
    Saarika updated LotDistance.java

--- LotDistance.java
+++ LotDistance.java
@@ -1,1 +1,29 @@
+import java.util.*;
+import java.lang.*;
 
+/* LotDistance is used to store the names of the parking lots on GMU FFX campus and their corresponding distances to the user's 
+  target building. Objects are used in DistanceFinder */
+public class LotDistance {
+ 
+  /* lot_name is the name of the parking lot which has a distance to the user's target building*/
+  private String lot_name;
+  
+  /* distance is the  distance of a parking lot  named lot_name to the user's target building*/
+  private double distance;
+  
+  /* Constructor initializes lot_name and distance fields*/
+  public LotDistance(String lot_name, double distance) {
+    this.lot_name = lot_name;
+    this.distance = distance;
+  }
+  
+  /* getName() returns the name of the parking lot (lot_name)*/
+  public String getName() {
+    return lot_name;
+  }
+  
+  /* getDistance() returns the distance*/
+  public double getDistance() {
+    return distance;
+  }
+}
