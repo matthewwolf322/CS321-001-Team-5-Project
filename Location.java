3631bd3b3d190db224a803639e2315e0a52b653f
sputla2 <56198446+sputla2@users.noreply.github.com>
2019-11-04 16:40:34 -0500

    Initial update to Location.java
    Saarika updated Location.java

--- Location.java
+++ Location.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class Location {
+  private String name;
+  private Coordinate coord;
+  
+  public Location(String name, Coordinate coord) {
+    this.name = name;
+    this.coord = coord;
+  }
+  
+  public Coordinate getCoordinates() {
+    return coord;
+  }
+  
+  public String getName() {
+    return name;
+  } 
+}
+    
