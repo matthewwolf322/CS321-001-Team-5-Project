d6ea2f83b8fc0702eee1e7de12374ef25a983fc0
showar2 <56351576+showar2@users.noreply.github.com>
2019-11-04 21:43:56 -0500

    Update Permit

--- Permit.java
+++ Permit.java
@@ -1,26 +1,40 @@
 import java.util.*;
 
-public class Permit
-{
+public class Permit{
+  
   private String title; // the name of the Permit
   private String [] avaliableLots; //array of all lots avaliable 
   
-  public Permit(String title, String [] avaliableLots)
-  {
+  public Permit(String title, String [] avaliableLots){
     this.title = title;
     this.avaliableLots = avaliableLots;
   }
   
-  public String getTitle()
-  {
+  public String getTitle(){
     return title;
   }
   
-  public String [] getLots()
-  {
+  public String [] getLots(){
     return avaliableLots;
   }
   
+  public String toString(){ 
+    String retr = "";
+    for (int i = 0; i < this.avaliableLots.length; i++){
+      retr = retr + this.avaliableLots[i] +", ";
+    }
+    return this.getTitle() + ": " + retr;  
+  }
+  
+ // public static void main(String [] args){ //for testing purposes
+    //test code here
+    //String [] list = new String [3];
+    //list[0] = "A";
+    //list[1] = "B";
+    //list[2] = "C";
+    //Permit A = new Permit("Permit 1",list);
+    //System.out.print(A.toString());
+  }
 }
   
   
