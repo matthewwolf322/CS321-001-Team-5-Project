ceb308084b14133ba42e8e0d947d999806bebe18
showar2 <56351576+showar2@users.noreply.github.com>
2019-11-04 21:09:26 -0500

    Update Coordinate
    create Coordinate class

--- Coordinate.java
+++ Coordinate.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class Coordinate {
+  private double latitude;
+  private double longitude;
+  
+  public Location(double latitude, double longitude) {//create new Coordinate object
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+  
+  public double getLat(){ //return latitude
+    return this.latitude;
+  }
+  
+  public double getLong(){ //return longitude
+    return this.longitude
+  }
+  
