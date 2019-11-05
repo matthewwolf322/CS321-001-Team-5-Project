092f3dd742d96da4cd8f90fefa80635b08d057ce
showar2 <56351576+showar2@users.noreply.github.com>
2019-11-04 20:44:45 -0500

    Update Permit
    Basic outline of Permit

--- Permit.java
+++ Permit.java
@@ -1,1 +1,21 @@
-
+public class Permit
+{
+  String title; // the name of the Permit
+  String [] avaliableLots; //array of all lots avaliable 
+  
+  public Permit(String title, String [] avaliableLots)
+  {
+    this.title = title;
+    this.avaliableLots = avaliableLots;
+  }
+  
+  public String getTitle()
+  {
+    return title;
+  }
+  
+  
+}
+  
+  
+  
