dbc91d4e678477adad1ef32db8eb6954f67f37df
matthewwolf322 <56235217+matthewwolf322@users.noreply.github.com>
2019-11-08 14:56:21 -0500

    Skeleton code
    just some skeleton code for reference, may need to add more methods. Also, implementation can be done a thousand different ways, and I don't exactly know which is the easiest and most effective.

--- BestLotFinder.java
+++ BestLotFinder.java
@@ -2,3 +2,25 @@
  * add a helper method that will take a permit and all of the lots, and return only lots associated with that permit
  * this method will be used in the main findBestLot method.
  */
+
+public class BestLotFinder {
+	// Some variables need to be established, not entirely sure how many
+	
+	//main workhorse of this class, returns sorted list of lot distances for use by display
+	public LotDistance[] findBestLot() {
+		
+	}
+	//this should be called by the display class before use
+	//we may want an assocaited boolean that doesn't allow use of the class until load has been called
+	public void load() {
+		
+	}
+	
+	// may want to have a string parameter that is the permit title
+	//alternatively we have a static varaible we reference to do it
+	//all depends on how one wants to implement it.
+	public Location[] lotsFromPermit() {
+		
+	}
+	
+}
