2d6f0dbfa3659aaf1ebfe3849daadc49ca5afdd3
matthewwolf322 <56235217+matthewwolf322@users.noreply.github.com>
2019-11-08 11:17:18 -0500

    Tasks for this file

--- BestLotFinder.java
+++ BestLotFinder.java
@@ -1,1 +1,4 @@
-
+/* TODO: add a load() method to load all the data
+ * add a helper method that will take a permit and all of the lots, and return only lots associated with that permit
+ * this method will be used in the main findBestLot method.
+ */
