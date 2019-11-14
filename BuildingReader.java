47fe6138c902e5ba8767e1516ab981f9bbaac035
matthewwolf322 <56235217+matthewwolf322@users.noreply.github.com>
2019-11-14 10:48:12 -0500

    Update BuildingReader.java

--- BuildingReader.java
+++ BuildingReader.java
@@ -17,7 +17,7 @@ public class BuildingReader {
 			sc = new Scanner(infile);
 		} catch (FileNotFoundException e) {
 			//Exit if file is not found
-			System.out.println("Error loading file " + inFile);
+			System.out.println("Error loading file " + infile);
 			System.exit(0);
 		}
 		
